09d3f556d70a38f86f219cbcbbe8190f29a0103d
nn <marianne.delio@ac-nancy-metz.fr>
2025-10-10 10:20:29 +0200

    Ajout assert fournisseur

--- src/Entity/Fournisseur.php
+++ src/Entity/Fournisseur.php
@@ -2,10 +2,9 @@
 
 namespace App\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use App\Repository\FournisseurRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: FournisseurRepository::class)]
 class Fournisseur
@@ -15,22 +14,57 @@ class Fournisseur
     #[ORM\Column]
     private ?int $id = null;
 
+    
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: 'Le nom est obligatoire')]
+    #[Assert\Length(
+        min: 3,
+        max: 255,
+        minMessage: 'Le nom doit comporter au moins {{ limit }} caractères',
+        maxMessage: 'Le nom ne peut pas dépasser {{ limit }} caractères',
+    )]
     private ?string $nom = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: 'L\'adresse est obligatoire')]
+    #[Assert\Length(
+        min: 3,
+        max: 255,
+        minMessage: 'L\'adresse doit comporter au moins {{ limit }} caractères',
+        maxMessage: 'L\'adresse ne peut pas dépasser {{ limit }} caractères',
+    )]
     private ?string $adresse = null;
 
     #[ORM\Column(length: 255, nullable: true)]
+    #[Assert\Length(
+        min: 3,
+        max: 255,
+        minMessage: 'Le mail doit comporter au moins {{ limit }} caractères',
+        maxMessage: 'Le mail ne peut pas dépasser {{ limit }} caractères',
+    )]
     private ?string $mail = null;
 
     #[ORM\Column(length: 255, nullable: true)]
+    #[Assert\Length(
+        min: 3,
+        max: 255,
+        minMessage: 'Le téléphone doit comporter au moins {{ limit }} caractères',
+        maxMessage: 'Le télépohne ne peut pas dépasser {{ limit }} caractères',
+    )]
     private ?string $telephone = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: 'Le nom du contact est obligatoire')]
+    #[Assert\Length(
+        min: 3,
+        max: 255,
+        minMessage: 'Le nom du contact doit comporter au moins {{ limit }} caractères',
+        maxMessage: 'Le nom du contact ne peut pas dépasser {{ limit }} caractères',
+    )]
     private ?string $nomContact = null;
 
     #[ORM\Column]
+    #[Assert\NotBlank(message: 'La date est obligatoire')]
     private ?\DateTime $dateEntree = null;
 
     public function getId(): ?int
