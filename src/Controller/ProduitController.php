5a44db4c96987b5fd5f5c440d29c4de094d0b2e8
Leandro <leandroveyrenc@gmail.com>
2025-11-12 09:47:55 +0100

    Sprint 4 mission 1$

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -5,24 +5,85 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ProduitRepository;
 use App\Entity\Produit;
-use App\Entity\Categorie;
+use App\Form\ProduitType;
+use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\Persistence\ManagerRegistry;
-
 
 
 final class ProduitController extends AbstractController
-{ 
+{
 
-    #[Route('/produit', name: 'app_produit')]
-    public function index(): Response
+    #[Route('/produit', name: 'app_produit', methods: ['GET'])]
+    public function index(ProduitRepository $repository): Response
     {
+        $lesProduits = $repository->findAll();
         return $this->render('produit/index.html.twig', [
-            'controller_name' => 'ProduitController',
+            'lesProduits' => $lesProduits,
         ]);
     }
 
+    #[Route('/produit/ajouter', name: 'app_produit_ajouter', methods: ['POST', 'GET'])]
+    public function ajouter(Produit $produit, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ProduitType::class, $produit);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // cas où le formulaire d'ajout a été soumis par l'utilisateur 
+            $produit = $form->getData();
+            // on met à jour la base de données 
+            $entityManager->persist($produit);
+            $entityManager->flush();
+            $this->addFlash(
+                'success',
+                'Le produit ' . $produit->getLibelle() . ' a été ajouté.'
+            );
+            return $this->redirectToRoute('app_produit');
+        } else {
+            // cas où l'utilisateur a demandé l'ajout, on affiche le formulaire d'ajout
+            return $this->render('produit/ajouter.html.twig', [
+                'form' => $form,
+            ]);
+        }
+    }
+
+    #[Route('/produit/modifier/{id<\d+>}', name: 'app_produit_modifier', methods: ['POST', 'GET'])]
+    public function modifier(Produit $produit, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ProduitType::class, $produit);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // cas où le formulaire  a été soumis par l'utilisateur et est valide
+            //pas besoin de "persister" l'entité : en effet, l'objet a déjà été retrouvé à partir de Doctrine ORM.
+            $entityManager->flush();
+            $this->addFlash(
+                'success',
+                'Le produit ' . $produit->getLibelle() . ' a été modifié.'
+            );
+
+            return $this->redirectToRoute('app_produit');
+        }
+        // cas où l'utilisateur a demandé la modification, on affiche le formulaire pour la modification
+        return $this->render('produit/modifier.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/produit/supprimer/{id<\d+>}', name: 'app_produit_supprimer', methods: ['GET'])]
+    public function supprimer(Produit $produit, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('action-item' . $produit->getId(), $request->get('_token'))) {
+            $entityManager->remove($produit);
+            $entityManager->flush();
+            $this->addFlash(
+                'success',
+                'Le produit ' . $produit->getLibelle() . ' a été supprimé.'
+            );
+        }
+        return $this->redirectToRoute('app_produit');
+    }
+
 }
-
-
