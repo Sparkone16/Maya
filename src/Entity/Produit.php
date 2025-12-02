058abd4b9debdd572f5bc2345786691b18c1f00c
Leandro <leandroveyrenc@gmail.com>
2025-12-02 15:43:37 +0100

    Image dans produit

--- src/Entity/Produit.php
+++ src/Entity/Produit.php
@@ -15,8 +15,12 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
 
 #[ORM\Entity(repositoryClass: ProduitRepository::class)]
+#[Vich\Uploadable]
 class Produit
 {
     #[ORM\Id]
@@ -80,6 +84,19 @@ class Produit
     #[Assert\Type("\DateTime")]
     #[Assert\Range(minPropertyPath: "debutDisponibilite")]
     private ?\DateTime $finDisponibilite = null;
+
+    #[Vich\UploadableField(mapping: 'categories', fileNameProperty: 'imageNom', size: 'imageTaille')]
+    private ?File $imageFichier = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?string $imageNom = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $imageTaille = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $imageDateMaj = null;
+
 
     public function getId(): ?int
     {
@@ -237,5 +254,50 @@ class Produit
 
         return $this;
     }
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+    public function setImageFichier(?File $imageFichier = null): void
+    {
+        $this->imageFichier = $imageFichier;
+
+        if (null !== $imageFichier) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->imageDateMaj = new \DateTimeImmutable();
+        }
+    }
+   
+    public function getImageFichier(): ?File
+    {
+        return $this->imageFichier;
+    }
+
+    public function setImageNom(?string $imageNom): void
+    {
+        $this->imageNom = $imageNom;
+    }
+
+    public function getImageNom(): ?string
+    {
+        return $this->imageNom;
+    }
+
+    public function setImageTaille(?int $imageTaille): void
+    {
+        $this->imageTaille = $imageTaille;
+    }
+
+    public function getImageTaille(): ?int
+    {
+        return $this->imageTaille;
+    }
+
 
 }
