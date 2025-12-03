b0c47b7b2a08637868be5c0de168618ccdcadbb1
Leandro <leandroveyrenc@gmail.com>
2025-12-03 11:04:00 +0100

    Fin de sprint 5

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -1,8 +1,8 @@
 <?php
-
 namespace App\Controller;
 
 use App\Repository\CategorieRepository;
+use App\Repository\EvenementRepository; // Ajout de l'import
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,12 +10,19 @@ use Symfony\Component\Routing\Attribute\Route;
 final class AccueilController extends AbstractController
 {
     #[Route('/accueil', name: 'app_accueil')]
-    public function index(CategorieRepository $repository): Response
+    public function index(CategorieRepository $categorieRepository, EvenementRepository $evenementRepository): Response
     {
         // lire les catégories
-        $lesCategories = $repository->findAll();
+        $lesCategories = $categorieRepository->findAll();
+        
+        // Récupérer les événements
+        $derniersEvenementsPasses = $evenementRepository->findLastThreePastEvents();
+        $prochainsEvenements = $evenementRepository->findNextThreeUpcomingEvents();
+        
         return $this->render('accueil/index.html.twig', [
             'lesCategories' => $lesCategories,
+            'derniersEvenementsPasses' => $derniersEvenementsPasses,
+            'prochainsEvenements' => $prochainsEvenements,
         ]);
     }
 }
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,23 +6,29 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Psr\Log\LoggerInterface;
+
+use function PHPUnit\Framework\isEmpty;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, LoggerInterface $logger): Response
     {
         // si après la première étape (login et mdp) nous avons un user
         // alors redirection vers la page de saisie du code d'authentification de Google Authenticator
         if ($this->getUser()) {
             // dans security.yaml, nous avons indiqué la route 2fa_login 
             // dans scheb_2fa.yaml, nous avons indiqué le template: security/2fa_form.html.twig 
+            $logger->info('Connexion reussie sans 2fa pour l\'utilisateur ' . $this->getUser()->getUserIdentifier());
             return $this->redirectToRoute('2fa_login');
         }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-
+        if($error) {
+            $logger->error('Erreur de connexion, raison : ' . $error->getMessage());
+        }
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
